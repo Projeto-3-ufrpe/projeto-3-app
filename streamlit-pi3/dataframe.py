52eeaf5c97464b978f1157656c2821ba6f67c0fb
Alexandre Melo <xandymelo@outlook.com.br>
2022-08-13 21:28:19 -0300

    desenvolvimento do algoritmo de regressao Logística

--- streamlit-pi3/dataframe.py
+++ streamlit-pi3/dataframe.py
@@ -1,5 +1,23 @@
 import pandas as pd
+import numpy as np
+def tratamento_csv(dataframe,list_columns):
+    #nas colunas booleanas trocar 'sim' e 'não' por 0 e 1
+    for column in list_columns:
+        if column == 'Diabetic':
+            dataframe[column] = np.where(dataframe[column] == 'No, borderline diabetes', 0, dataframe[column])
+            dataframe[column] = np.where(dataframe[column] == 'Yes (during pregnancy)', 1, dataframe[column])
+        dataframe[column] = np.where(dataframe[column] == 'Yes', 1, dataframe[column])
+        dataframe[column] = np.where(dataframe[column] == 'No', 0, dataframe[column])
+        dataframe[f'{column}'] = pd.to_numeric(dataframe[f'{column}'])
+    return dataframe
+
 class Dados:
-    dataframe = pd.read_csv('../heart_2020_cleaned.csv')
+    dataframe = pd.read_csv('./heart_2020_cleaned.csv')
+    dataframe = tratamento_csv(dataframe, ['HeartDisease', 'Smoking', 'AlcoholDrinking', 'KidneyDisease', 'SkinCancer', 'Asthma', 'Diabetic', 'PhysicalActivity','DiffWalking','Stroke'])
+    dataframe_somente_com_colunas_numericas = dataframe.drop(columns=['AgeCategory','GenHealth', 'Race','Sex', ])
+    dataframe['SkinCancer'] = np.where(dataframe['SkinCancer'] == 'Yes', 1, dataframe['SkinCancer'])
+    dataframe['SkinCancer'] = np.where(dataframe['SkinCancer'] == 'No', 0, dataframe['SkinCancer'])
     df_somente_com_doencas_do_coracao = dataframe.query("HeartDisease == 'Yes'")
     df_somente_sem_doencas_do_coracao = dataframe.query("HeartDisease == 'No'")
+
+
--- streamlit-pi3/regressaologistica.py
+++ streamlit-pi3/regressaologistica.py
@@ -3,15 +3,43 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from dataframe import Dados
-# %matplotlib inline
+#biblioteca que auxilia padronizar os dados para aplicar a regressão logística
+import sklearn.preprocessing as sk
 
-df = Dados.dataframe
+#Criar x0's com valores iguais a 1 (?)
+def insert_notes(X):
+    ones = np.ones([X.shape[0], 1])
+    return np.concatenate((ones, X), axis=1)
 
-#separar a quantidade de feature
-features = len(df.columns - 1)
-print(features)
+#função que faz que a nossa regressão possa ser aplicada em um problema de classificação
+def sigmoid(z):
+    pass
 
-# training_data = np.array(df.drop(''))
+    
+
+df = Dados.dataframe_somente_com_colunas_numericas
+#pegar o número da coluna booleana, no nosso caso, SkinCancer
+features = len(df.columns) - 1
+# training_data = os dados em formato de lista sem a coluna passada no parâmetro de df.drop()
+training_data = np.array(df.drop('SkinCancer', axis=1))
+# target_variable = pegando os dados da coluna target em forma de lista também
+target_variable = df.iloc[:,features:features + 1 ].values
+#precisamos somente de valores número em todas as colunas, fazer esse tratamento da média
+media_valores = training_data.mean(axis=0)
+desvio_padrao = training_data.std(axis=0)
+#padronizar os dados para aplicação da regressão logística
+scaler = sk.StandardScaler()
+scaler.fit(training_data)
+training_data = scaler.transform(training_data)
+#criando uma matriz do tamanho de uma linha da tabela com valores aleatórios entre 0 e 1
+linha_w = np.random.rand(1, features+1)
+print(linha_w)
+
+
+
+
+
+
 
 
 
