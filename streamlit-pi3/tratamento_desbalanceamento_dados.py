944fb8f9f64db534a64165bd77febae92768aeeb
Alexandre Melo <xandymelo@outlook.com.br>
2022-08-18 02:20:29 -0300

    Update tratamento_desbalanceamento_dados.py

--- streamlit-pi3/tratamento_desbalanceamento_dados.py
+++ streamlit-pi3/tratamento_desbalanceamento_dados.py
@@ -1,15 +1,13 @@
 import streamlit as st
 import dataframe
 import sklearn
+import pandas as pd
 import numpy as np
-from sklearn import model_selection, dummy, metrics
+from sklearn import model_selection, dummy, metrics, utils, linear_model
 def desbalanceamento_dados():
     st.title("A Partir dos dados abaixo podemos notar que há um desbalanceamento dos dados")
     df = dataframe.Dados.dataframe
-    quantidade_dados_com_doencas_coracao = df['HeartDisease'].loc[df['HeartDisease'] == 1].count()
-    quantidade_dados_sem_doencas_coracao = df['HeartDisease'].loc[df['HeartDisease'] == 0].count()
-    st.subheader('# Quantidade de Dados com doenças do coração - ' + str(quantidade_dados_com_doencas_coracao))
-    st.subheader('# Quantidade de Dados sem doenças do coração - ' + str(quantidade_dados_sem_doencas_coracao))
+    st.dataframe(df.HeartDisease.value_counts())
     #testando com DummyClassifier
     y = df.HeartDisease
     X = df.drop('HeartDisease', axis=1)
@@ -22,4 +20,23 @@ def desbalanceamento_dados():
     st.subheader('# Unique predicted labels: {}'.format(np.unique(dummy_pred)))
     # checking accuracy
     st.subheader('# Test score: {}'.format(metrics.accuracy_score(y_test, dummy_pred)))
+    st.subheader("# Esta alta taxa implica que os dados estão desbalanceados, logo precisamos resolver isso!")
+    st.subheader("#Para isto, vamos usar a técnina de Classe minoritária sobreamostra")
+    st.subheader("Referência: https://towardsdatascience.com/methods-for-dealing-with-imbalanced-data-5b761be45a18#:~:text=Imbalanced%20classes%20are%20a%20common%20problem%20in%20machine%20learning%20classification,spam%20filtering%2C%20and%20fraud%20detection")
+    X = pd.concat([X_train, y_train], axis=1)
+    sem_doencas_do_coracao = X[X.HeartDisease==0]
+    com_doencas_do_coracao = X[X.HeartDisease==1]
+    com_doencas_expandido = utils.resample(com_doencas_do_coracao,
+                          replace=True, # sample with replacement
+                          n_samples=len(sem_doencas_do_coracao), # match number in majority class
+                          random_state=27) # reproducible results
+    combinacao_expandido_com_sem_doencas = pd.concat([sem_doencas_do_coracao, com_doencas_expandido])
+    st.dataframe(combinacao_expandido_com_sem_doencas.HeartDisease.value_counts())
+    # st.dataframe(combinacao_expandido_com_sem_doencas)
+    #TESTANDO A REGRESSAO LOGISTICA NOS DADOS
+    subamostragem = linear_model.LogisticRegression(solver='liblinear').fit(X_train, y_train)
+    subamostragem_pred = subamostragem.predict(X_test)
+    #AQUI ESTÁ DANDO ERRO PORQUE TEMPOS QUE USAR SOMENTE AS COLUNAS NUMÉRICAS
+    st.subheader(metrics.accuracy_score(y_test, subamostragem_pred))
+    
 
